56473eaa4cd34e65a8e74f2afd9881ed53eea687
SeLub <siteup24@gmail.com>
2022-01-04 18:48:09 +0300

    fix: logLevel setup

--- dist/logger.js
+++ dist/logger.js
@@ -1,10 +1,10 @@
 import { pino } from 'pino';
 import { LOG_LEVEL } from './common/config.js';
-let log_level = LOG_LEVEL ? LOG_LEVEL : 'info';
+const logLevel = LOG_LEVEL ? LOG_LEVEL : 'info';
 const transport = pino.transport({
     targets: [
-        { level: log_level, target: 'pino/file', options: { destination: 2, translateTime: 'SYS:standard' } },
-        { level: log_level, target: 'pino/file', options: { destination: './logs/full.log', mkdir: true } },
+        { level: logLevel, target: 'pino/file', options: { destination: 2, translateTime: 'SYS:standard' } },
+        { level: logLevel, target: 'pino/file', options: { destination: './logs/full.log', mkdir: true } },
         { level: 'error', target: 'pino/file', options: { destination: './logs/error.log', mkdir: true } }
     ],
 });
